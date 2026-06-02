835eb9f0eacb472fedec87e1b90241d254f32984
KishChaitu <netrapremchandu@gmail.com>
2026-06-02 08:11:33 +0000

    Updated backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,3 @@
-// Backend configured to use GCS. Replace `my-bucket` and `path` with your values.
 terraform {
-  backend "gcs" {
-    bucket = var.remote_state_bucket
-    prefix = var.remote_state_prefix
-  }
+  backend "azurerm" {}
 }
