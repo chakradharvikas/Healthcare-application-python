0f9f75ceb8c2baadbe86ca911db4bfa357aa2bb2
KishChaitu <netrapremchandu@gmail.com>
2026-06-02 16:40:04 +0000

    Updated outputs.tf

--- terraform/azure/outputs.tf
+++ terraform/azure/outputs.tf
@@ -1,6 +1,6 @@
-output "acr_login_server" {
-  value = module.acr.login_server
-}
+# output "acr_login_server" {
+#   value = module.acr.login_server
+# }
 
 output "key_vault_id" {
   value = module.keyvault.key_vault_id
